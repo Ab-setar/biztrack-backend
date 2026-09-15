fea8826f558f7561e38dbb5338f3fd0f87e4b41e
Abdusetar <fati40458689@gmail.com>
2026-09-16 01:15:17 +0300

    feat: add product read endpoints

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -26,3 +26,44 @@ export const createProduct = async (req, res) => {
     });
   }
 };
+export const getProducts = async (req, res) => {
+    try {
+        const result = await pool.query(
+            `SELECT * FROM products`
+        );
+
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to get products"
+        });
+    }
+};
+export const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            `SELECT * FROM products
+             WHERE id = $1`,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json(result.rows[0]);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to get product"
+        });
+    }
+};
