d738191564538d42dff426d7a9e8ffe76e8a46fe
Abdusetar <fati40458689@gmail.com>
2026-09-19 20:03:58 +0300

    feat: complete product CRUD API

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -67,3 +67,71 @@ export const getProductById = async (req, res) => {
         });
     }
 };
+export const updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const { name, price, stock_quantity } = req.body;
+
+        if (!name || price < 0 || stock_quantity < 0) {
+            return res.status(400).json({
+                message: "Invalid product data"
+            });
+        }
+
+        const result = await pool.query(
+            `UPDATE products
+             SET name = $1,
+                 price = $2,
+                 stock_quantity = $3
+             WHERE id = $4
+             RETURNING *`,
+            [name, price, stock_quantity, id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json(result.rows[0]);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to update product"
+        });
+    }
+};
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            `DELETE FROM products
+             WHERE id = $1
+             RETURNING *`,
+            [id]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Product deleted successfully",
+            product: result.rows[0]
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Failed to delete product"
+        });
+    }
+};
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,7 +3,9 @@ import express from "express";
 import {
     createProduct,
     getProducts,
-    getProductById
+    getProductById,
+    updateProduct,
+    deleteProduct
 } from "../controllers/productController.js";
 
 const router = express.Router();
@@ -13,4 +15,6 @@ router.post("/", createProduct);
 router.get("/", getProducts);
 
 router.get("/:id", getProductById);
+router.put("/:id",updateProduct);
+router.delete("/:id", deleteProduct);
 export default router;
